2aaeac91e69bfef1212b6335ec4db2a08be0c35d
quentinxsy <quentinxsy@gmail.com>
2022-06-02 08:03:30 -0700

    1

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator move()
+    {
+        yield return new WaitForSeconds(10f);
+        this.GetComponent<Spin>().enabled = true;
+        yield return new WaitForSeconds(10f);
+        this.GetComponent<Spin>().enabled = false;
+
+    }
+}
