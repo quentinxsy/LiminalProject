834667c5184baa767fec271db5ab3f04ec1ea68c
quentinxsy <quentinxsy@gmail.com>
2022-06-02 15:57:13 -0700

    1

---
+++ Assets/CamFix.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFix : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera cam = GetComponent<Camera>();
+        cam.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
