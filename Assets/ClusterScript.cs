de31795c92fae90cf4fe5aa10bbe4b8ab624e221
quentinxsy <quentinxsy@gmail.com>
2022-06-09 15:12:39 -0700

    week10 in class

---
+++ Assets/ClusterScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClusterScript : MonoBehaviour
+{
+    public GameObject largeAtom;
+    public GameObject rings;
+    public GameObject cluster;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void TurnOn()
+    {
+        cluster.SetActive(true);
+        largeAtom.SetActive(true);
+        rings.SetActive(true);
+    }
+
+
+
+}
